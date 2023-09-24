02389c9124545e29845e8fe44d0c252861539b9b
EstebanPastor <estebanandrespastor@gmail.com>
2023-09-24 11:32:57 -0300

    feat: :sparkles: Add row prices and input
    Hardcoded input prices and rows for prices

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-
+import AmountInput from "./components/AmountInput";
+import { useState } from "react";
+import ResultRow from "./components/ResultRow";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [amount, setAmount] = useState("100");
 
-export default App
+  return (
+    <main className="max-w-2xl mx-auto px-4 py-8">
+      <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">
+        Find cheapest BTC
+      </h1>
+      <div className="flex justify-center mt-6 ">
+        <AmountInput
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </div>
+      <div className="mt-6">
+        <ResultRow />
+        <ResultRow />
+        <ResultRow />
+        <ResultRow />
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/AmountInput.tsx
@@ -0,0 +1,15 @@
+import InputOffer, { InputProps } from "./InputOffer";
+
+export default function AmountInput(props: InputProps) {
+  return (
+    <div className="flex items-center bg-blue-950 border border-white/10 rounded-lg">
+      <InputOffer
+        placeholder="Enter amount"
+        className="border-0 w-24 pl-4 bg-transparent text-2xl"
+        value={props.value}
+        onChange={props.onChange}
+      />
+      <span className="text-white/50 px-4">USD</span>
+    </div>
+  );
+}
